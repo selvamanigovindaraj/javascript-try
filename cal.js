9f77b0fdcaa5a62752aa50f8471fbd08a9895aef
unknown <selvamanigovindaraj@outlook.com>
2017-11-19 15:44:51 +0530

    tries

---
+++ cal.js
@@ -0,0 +1,29 @@
+<script type="text/javascript">
+function cals(buttonValue) 
+{
+if (buttonValue == 'C') 
+{
+   document.getElementById('valueshow').value = '0';
+}
+else
+  {
+    if(document.getElementById('valueshow').value == '0') 
+ {
+  
+   document.getElementById('valueshow').value = buttonValue;
+}
+  
+  else
+   {
+    
+    document.getElementById('valueshow').value += buttonValue;
+   }
+  }
+ }
+  function cal(equation)  
+   
+  {
+  var answer = eval(equation);
+document.getElementById('valueshow').value = answer;
+ }
+ </script>
